dfdfa683ad28e20ce20b6139e25a6e65d4dcde2d
Jhojan <jhojanrock98@gmail.com>
2023-11-23 17:21:18 -0500

    Implementación de Fluro en Cupertino

--- lib/main.dart
+++ lib/main.dart
@@ -1,98 +1,32 @@
-//import 'package:flutter/material.dart';
-//import 'package:mi_primera_pagina_web/ui/pages/home_page.dart';
-//
-//void main() => runApp(const MyApp());
-//
-//class MyApp extends StatelessWidget {
-//  const MyApp({super.key});
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      debugShowCheckedModeBanner: false,
-//      title: 'Material App',
-//      home: Scaffold(
-//        appBar: AppBar(
-//          title: const Text('Material App Bar'),
-//        ),
-//        body: const HomePage()
-//      ),
-//    );
-//  }
-//}
-//
-
 import 'package:flutter/cupertino.dart';
+import 'package:mi_primera_pagina_web/routers/router.dart';
 
-/// Flutter code sample for [CupertinoPageScaffold].
 
-void main() => runApp(const PageScaffoldApp());
+void main() => runApp(const MyApp());
 
-class PageScaffoldApp extends StatelessWidget {
-  const PageScaffoldApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const CupertinoApp(
-      theme: CupertinoThemeData(brightness: Brightness.light),
-      home: PageScaffoldExample(),
-    );
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  @override
+  void initState() {
+    super.initState();
+    Flurorouter.configureRoutes();
   }
-}
-
-class PageScaffoldExample extends StatefulWidget {
-  const PageScaffoldExample({super.key});
-
-  @override
-  State<PageScaffoldExample> createState() => _PageScaffoldExampleState();
-}
-
-class _PageScaffoldExampleState extends State<PageScaffoldExample> {
-  int _count = 0;
 
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      backgroundColor: CupertinoColors.systemPink,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Center(
-              child: Text('You have pressed the button $_count times.'),
-            ),
-            const SizedBox(height: 20.0),
-            Center(
-
-              child: CupertinoButton.filled(
-              child: const Icon(CupertinoIcons.add),
-               onPressed: () {
-                    Navigator.of(context).push(
-                      CupertinoPageRoute<void>(
-                        builder: (BuildContext context) {
-                          return const Home();
-                        },
-                      ),
-                    );
-                  },
-                //child: const Icon(CupertinoIcons.add),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return  CupertinoPageScaffold(
-      child: Center(),
+    return  CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      theme: const CupertinoThemeData(brightness: Brightness.light),
+      title: 'Cupertino App',
+      initialRoute: '/home',
+      onGenerateRoute: Flurorouter.router.generator,
     );
   }
 }
