a498c80160a865fe3901198617a7c14f02b604f4
Jhojan <jhojanrock98@gmail.com>
2023-11-24 19:29:56 -0500

    Implementación y configuración  de AnimatedIcon

--- lib/ui/shared/custom_app_menu.dart
+++ lib/ui/shared/custom_app_menu.dart
@@ -1,16 +1,44 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class CustomAppMenu extends StatelessWidget {
+class CustomAppMenu extends StatefulWidget {
   const CustomAppMenu({super.key});
+
+  @override
+  State<CustomAppMenu> createState() => _CustomAppMenuState();
+}
+
+class _CustomAppMenuState extends State<CustomAppMenu> with  SingleTickerProviderStateMixin{
+
+  bool isOpen = false;
+  late AnimationController? controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return  MouseRegion(
       cursor: SystemMouseCursors.click,
       child: GestureDetector(
+
         onTap: (){
-          print('click en Menú');
+           if(isOpen){
+            controller!.forward();
+          } else {
+            controller!.reverse();
+          }
+
+          setState(() {
+            isOpen = !isOpen;
+          });
         },
         child: Container(
           padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
@@ -19,6 +47,11 @@ class CustomAppMenu extends StatelessWidget {
           color: CupertinoColors.black,
           child: Row(
             children: [
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+                width: isOpen ? 50 : 0,
+              ),
               Text(
                 'Menú',
                 style: GoogleFonts.robotoCondensed(
@@ -27,11 +60,12 @@ class CustomAppMenu extends StatelessWidget {
                 ),
               ),
               const Spacer(),
-              const Icon(
-                  CupertinoIcons.ellipsis_vertical,
-                  color: CupertinoColors.white,
-                  size: 20,
-                ),
+
+                AnimatedIcon(
+                 icon: AnimatedIcons.menu_close,
+                 progress: controller! ,
+                 color: CupertinoColors.white,
+                )
             ],
           ),
         ),
